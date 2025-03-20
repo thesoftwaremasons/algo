9bec8d46441cfc93c26916267ec25d98a59ccf8d
Babafemi Ogundipe <smiles2k6@gmail.com>
2025-03-20 17:51:08 +0100

    two sums

---
+++ twosum.js
@@ -0,0 +1,18 @@
+///Find a pair with the given sum in an array
+
+
+
+function twoSum(arr, target) {
+    let map = new Map();
+    for (let i = 0; i <= arr.length; i++) {
+        let diff = target - arr[i]
+        if (map.has(diff)) {
+            console.log(arr[i],diff)
+        } else {
+            map.set(arr[i],diff)
+        }
+    }
+}
+
+const arr = [8, 7, 2, 5, 3, 1];
+twoSum(arr,10)
